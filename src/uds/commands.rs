f32c9c3bb1f61979b2355299defa80dc115edfbe
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2026-08-17 17:14:13 -0700

    chore: update pyo3 and allow multiple ver due to syn

--- src/uds/commands.rs
+++ src/uds/commands.rs
@@ -19,30 +19,37 @@ python_test!(uds, UdsCommand, DiagnosticSessionControl, ECUReset);
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum UdsCommand {
     /// The client requests to control a diagnostic session with a server(s).
+    ///
     /// See also [`crate::uds::UdsSessionType`].
     DiagnosticSessionControl = 0x10,
     /// The client forces the server(s) to perform a reset.
+    ///
     /// See also [`crate::uds::ResetType`].
     ECUReset = 0x11,
     /// The client requests to unlock a secured server(s).
+    ///
     /// See also [`crate::uds::SecurityOperation`].
     SecurityAccess = 0x27,
     /// The client controls the setting of communication parameters in the server (e.g. communication baud-rate).
+    ///
     /// See also [`crate::uds::CommunicationLevel`].
     CommunicationControl = 0x28,
     /// The client indicates to the server(s) that it is still present.
     TesterPresent = 0x3E,
     /// The client requests to access the server(s) with a specific access mode.
+    ///
     /// See also [`crate::uds::AuthenticationSubFunction`].
     Authentication = 0x29,
     /// The client uses this service to perform data transmission with an extended data link security.
     SecuredDataTransmission = 0x84,
     /// The client controls the setting of DTCs in the server.
+    ///
     /// See also [`crate::uds::DtcSettingType`].
     ControlDTCSetting = 0x85,
     /// The client requests to set up and/or control an event mechanism in the server.
     ResponseOnEvent = 0x86,
     /// The client requests control of the communication baud-rate.
+    ///
     /// See also [`crate::uds::LinkControlType`].
     LinkControl = 0x87,
     /// The client requests to read the current value of a record identified by a provided dataIdentifier.
@@ -52,9 +59,11 @@ pub enum UdsCommand {
     /// The client requests to read the scaling information of a record identified by a provided dataIdentifier.
     ReadScalingDataByIdentifier = 0x24,
     /// The client requests to schedule data in the server for periodic transmission.
+    ///
     /// See also [`crate::uds::PeriodicTransmissionMode`].
     ReadDataByPeriodicIdentifier = 0x2A,
     /// The client requests to dynamically define data identifiers that may subsequently be read by the `ReadDataByIdentifier` service.
+    ///
     /// See also [`UdsCommand::ReadDataByIdentifier`] and [`crate::uds::DataDefinitionType`].
     DynamicallyDefineDataIdentifier = 0x2C,
     /// The client requests to write a record specified by a provided dataIdentifier.
@@ -64,12 +73,15 @@ pub enum UdsCommand {
     /// Allows the client to clear diagnostic information from the server (including DTCs, captured data, etc.).
     ClearDiagnosticInformation = 0x14,
     /// Allows the client to request diagnostic information from the server (including DTCs, captured data, etc.).
+    ///
     /// See also [`crate::uds::DtcSubFunction`].
     ReadDTCInformation = 0x19,
     /// The client requests the control of an input/output specific to the server.
+    ///
     /// See also [`crate::uds::InputOutputControlParameter`].
     InputOutputControlByIdentifier = 0x2F,
     /// The client requests to start, stop a routine in the server(s) or requests the routine results.
+    ///
     /// See also [`crate::uds::RoutineControlType`].
     RoutineControl = 0x31,
     /// The client requests the negotiation of a data transfer from the client to the server.
@@ -81,6 +93,7 @@ pub enum UdsCommand {
     /// The client requests the termination of a data transfer.
     RequestTransferExit = 0x37,
     /// The client requests the negotiation of a file transfer between server and client.
+    ///
     /// See also [`crate::uds::FileTransferMode`].
     RequestFileTransfer = 0x38,
 }
